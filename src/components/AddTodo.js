66bce4f4a3f21a6cc18a0b2b199b5bdc13829d9f
TommmyKelly <tommy_kelly@icloud.com>
2021-02-12 22:32:34 +0000

    ADD: added warning toast message when input is blank and user tries to submit

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef, useContext } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Toast } from "react-bootstrap";
 import TodosContext from "../context/TodosContext";
 
 const AddTodo = () => {
+  const [show, setShow] = useState(false);
+
   const todosContext = useContext(TodosContext);
 
   const { addTodo } = todosContext;
@@ -15,7 +17,10 @@ const AddTodo = () => {
   };
 
   const AddNewTodo = () => {
-    if (Inputref.current.value === "") return;
+    if (Inputref.current.value === "") {
+      setShow(true);
+      return;
+    }
     const newTodo = {
       id: Date.now(),
       todo: input,
@@ -27,21 +32,37 @@ const AddTodo = () => {
   };
 
   return (
-    <Card className={"my-1 rounded "} style={{ width: "100%" }}>
-      <Card.Body className='d-flex justify-content-center align-items-center'>
-        <input
-          type='text'
-          style={{ width: "100%", marginRight: "4px" }}
-          onInput={(e) => onInput(e)}
-          ref={Inputref}
-          placeholder='Add Tood...'
-        />
+    <>
+      <Toast
+        style={{ backgroundColor: "pink" }}
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        autohide
+      >
+        <Toast.Header>
+          <img src='holder.js/20x20?text=%20' className='rounded mr-2' alt='' />
+          <strong className='mr-auto'>Todo List</strong>
+          <small>Input missing</small>
+        </Toast.Header>
+        <Toast.Body>Whoops, input is blank!</Toast.Body>
+      </Toast>
+      <Card className={"my-1 rounded "} style={{ width: "100%" }}>
+        <Card.Body className='d-flex justify-content-center align-items-center'>
+          <input
+            type='text'
+            style={{ width: "100%", marginRight: "4px" }}
+            onInput={(e) => onInput(e)}
+            ref={Inputref}
+            placeholder='Add Tood...'
+          />
 
-        <Button className='ml-1' variant='primary' onClick={AddNewTodo}>
-          <i className='fas fa-plus-square'></i>
-        </Button>
-      </Card.Body>
-    </Card>
+          <Button className='ml-1' variant='primary' onClick={AddNewTodo}>
+            <i className='fas fa-plus-square'></i>
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
